033bf2586b4504c5d87d61eca0ffddf82c9e73f4
Robbe De Bie <robbedebie1@gmail.com>
2023-06-08 22:02:07 +0200

    made soldiers follow the path correctly

--- PathSpawner.gd
+++ PathSpawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var path = preload("res://Levels/stage_1.tscn")
+@onready var path = preload("res://Levels/level1_path.tscn")
 func _on_timer_timeout():
 	var tempPath = path.instantiate()
 	add_child(tempPath)
