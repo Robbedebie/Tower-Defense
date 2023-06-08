d5a8bbeac6f4dd7e4fa85c8ecbaf0b81568b99b0
Robbe De Bie <robbedebie1@gmail.com>
2023-06-08 22:07:06 +0200

    added export value number of enemies where you can specify how many enemies need to be spawned

--- PathSpawner.gd
+++ PathSpawner.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+@export var number_of_enemies = 9
+var counter = 0
+
 @onready var path = preload("res://Levels/level1_path.tscn")
 func _on_timer_timeout():
-	var tempPath = path.instantiate()
-	add_child(tempPath)
+	counter += 1
+	if counter <= number_of_enemies:
+		var tempPath = path.instantiate()
+		add_child(tempPath)
