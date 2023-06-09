072425106ae00f6b8daabe9d7d20eccfec4cb237
janbr0 <121790863+janbr0@users.noreply.github.com>
2023-06-09 09:42:53 +0200

    coupled main menu to game

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,7 @@
 extends Node
 
-
 func _on_new_game_button_button_up():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_options_button_button_up():
 	get_tree().change_scene_to_file("res://options_menu.tscn")
