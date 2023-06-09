b7994504fd005f624cc8bcc84ade9f8ddd987e54
janbr0 <121790863+janbr0@users.noreply.github.com>
2023-06-09 10:47:24 +0200

    Added enemy counter

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var enemies = $PathSpawner.get_children()
+	if(enemies.size() == 1 && $PathSpawner.get_wave_finished()):
+		get_tree().change_scene_to_file("res://main_menu.tscn")
