a45bec122ebb4c03ee2502202dc374c33c6872c3
Robbe De Bie <robbedebie1@gmail.com>
2023-06-09 11:48:02 +0200

    placing turrets now works correctly

---
+++ Towers/RedTowerPanel.gd
@@ -0,0 +1,28 @@
+extends Panel
+
+@onready var tower = preload("res://Towers/red_bullet_tower.tscn")
+var currTile
+
+func _on_gui_input(event):
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#left click down
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		#left click down drag
+		get_child(1).global_position = event.global_position
+		
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		print("left mouse button up")
+		# get current  global_position of dragged tower 
+		var drop_pos = get_child(1).global_position
+		drop_pos.x -= 905
+		drop_pos.y -= 520 # were we want the tower
+		
+		get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Main/Towers")
+		path.add_child(tempTower)
+		# temp_tower.global_position = event.global_position
+		tempTower.global_position = drop_pos
+		tempTower.get_node("Area").hide()
